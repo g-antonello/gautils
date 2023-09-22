26e0f67e4b4e5335eb4d7b1631bee015a867adbf
g-antonello <giacomoantonello1@gmail.com>
2023-09-22 10:51:08 +0200

    New Function

--- R/SOLAR_results_kbl.R
+++ R/SOLAR_results_kbl.R
@@ -1,6 +1,8 @@
-#' Title
+#' Show clean SOLAR results
 #'
-#' @param x A \code{data.frame} object as retrieved by `get_SOLAR_results`, which is a useful function to retrieve multiple-trait heritability. But in principle it works even with just 1 trait
+#' Makes a kbl table from raw results obtained by by `get_SOLAR_results`, which is a useful function to retrieve multiple-trait heritability. But in principle it works even with just 1 trait
+#'
+#' @param x A \code{data.frame} object as retrieved as-is by `get_SOLAR_results`
 #' @param padj_method A \code{character} with the adjustment required. 'none' is also accepted and crude P-values will be kept
 #' @param complete.cases A \code{logical} saying whether you want to remove rows with 1 or more NAs. Default is `TRUE`
 #'
@@ -8,56 +10,57 @@
 #' @export
 #'
 #' @examples
-#' 
-#' No microbiome SOLAR runs were stored as data. 
-#' 
-function(x, padj_method = "BH", complete.cases = TRUE){
-  
+#'
+#' # No microbiome SOLAR runs were stored as data.
+#'
+
+SOLAR_results_kbl <- function(x, padj_method = "BH", complete.cases = TRUE){
+
   if(complete.cases){
     x_complete <- x[complete.cases(x),]
   }
-  
+
   if(padj_method == "none"){
-    
-    x2 <- x_complete %>% 
+
+    x2 <- x_complete %>%
       mutate(
         Estimate = paste0(round(h2, 3), " (", round(h2_SE, 3), ")"),
         Estimate2 = paste0(round(c2, 3), " (", round(c2_SE, 3), ")")
-      ) %>% 
-      select(trait, Estimate, h2_p, Estimate2, c2_p) %>% 
+      ) %>%
+      select(trait, Estimate, h2_p, Estimate2, c2_p) %>%
       mutate_if(is.numeric, format.pval, digits = 2) %>%
-      as_tibble() %>% 
-      as.matrix() %>% 
+      as_tibble() %>%
+      as.matrix() %>%
       set_colnames(c(" ", rep(c("Estimate (Std.Err.)", "P-value"),2)))
-    
+
   } else{
-    x_padj <- x_complete %>% 
+    x_padj <- x_complete %>%
       mutate(
         h2_padj = p.adjust(h2_p, padj_method),
         c2_padj = p.adjust(c2_p, padj_method)
-        
-        
+
+
       )
-    
-    x2 <- x_padj %>% 
+
+    x2 <- x_padj %>%
       mutate(
         Estimate = paste0(round(h2, 3), " (", round(h2_SE, 3), ")"),
         Estimate2 = paste0(round(c2, 3), " (", round(c2_SE, 3), ")")
-      ) %>% 
-      select(trait, Estimate, h2_padj, Estimate2, c2_padj) %>% 
+      ) %>%
+      select(trait, Estimate, h2_padj, Estimate2, c2_padj) %>%
       mutate_if(is.numeric, format.pval, digits = 1) %>%
-      as_tibble() %>% 
-      as.matrix() %>% 
+      as_tibble() %>%
+      as.matrix() %>%
       set_colnames(c("Trait", rep(c("Estimate (Std.Err.)", paste0("Q-value", " (", padj_method, ")")),2)))
   }
-  
-  
-  
-  kable_basic <- kbl(x2) %>% 
-    kable_styling() %>% 
+
+
+
+  kable_basic <- kbl(x2) %>%
+    kable_styling() %>%
     add_header_above(c(" " = 1, "h²" = 2, "c²" = 2))
-  
-  
+
+
   return(kable_basic)
-  
+
 }
