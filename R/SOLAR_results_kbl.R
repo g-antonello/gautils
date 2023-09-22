afff8290cabd849887236e6cf8c70a50c204de0d
g-antonello <giacomoantonello1@gmail.com>
2023-09-22 10:28:19 +0200

    major fix on function

--- R/SOLAR_results_kbl.R
+++ R/SOLAR_results_kbl.R
@@ -1,51 +1,63 @@
-#' Make a kable table of SOLAR results
+#' Title
 #'
-#' @param x An object obtained with \code{get_SOLAR_results} 
-#' @param highlight_significant \code{numeric} A heritability (h2) P-value threshold under which you would like to highlight the row
+#' @param x A \code{data.frame} object as retrieved by `get_SOLAR_results`, which is a useful function to retrieve multiple-trait heritability. But in principle it works even with just 1 trait
+#' @param padj_method A \code{character} with the adjustment required. 'none' is also accepted and crude P-values will be kept
+#' @param complete.cases A \code{logical} saying whether you want to remove rows with 1 or more NAs. Default is `TRUE`
 #'
-#' @import kableExtra
-#' @importFrom magrittr set_rownames set_colnames
-#' 
-#' @return a \code{kableExtra} object. ready to print as .html
+#' @return a \code{kbl} table, with default settings
 #' @export
 #'
 #' @examples
 #' 
-#' tmp <- get_SOLAR_results(path_to_h2_runs)
-#' print(SOLAR_results_kbl(tmp, 0.01))
-
-SOLAR_results_kbl <- function(x, highlight_significant = 0.05){
+#' No microbiome SOLAR runs were stored as data. 
+#' 
+function(x, padj_method = "BH", complete.cases = TRUE){
   
-  x <- relocate(x, 
-                h2_SE, 
-                .before = h2_p) %>% 
-    relocate(
-      c2_SE,
-      .before = c2_p
-    )
+  if(complete.cases){
+    x_complete <- x[complete.cases(x),]
+  }
   
-  x2 <- x %>% 
-    mutate(
-      Estimate = paste0(round(h2, 3), " (", round(h2_SE, 3), ")"),
-      Estimate2 = paste0(round(c2, 3), " (", round(c2_SE, 3), ")")
-    ) %>% 
-    select(trait, Estimate, h2_p, Estimate2, c2_p) %>% 
-    mutate_if(is.numeric, format.pval, digits = 2) %>%
-    as_tibble() %>% 
-    as.matrix() %>% 
-    set_colnames(c(" ", rep(c("Estimate (Std.Err.)", "P-value"),2)))
+  if(padj_method == "none"){
+    
+    x2 <- x_complete %>% 
+      mutate(
+        Estimate = paste0(round(h2, 3), " (", round(h2_SE, 3), ")"),
+        Estimate2 = paste0(round(c2, 3), " (", round(c2_SE, 3), ")")
+      ) %>% 
+      select(trait, Estimate, h2_p, Estimate2, c2_p) %>% 
+      mutate_if(is.numeric, format.pval, digits = 2) %>%
+      as_tibble() %>% 
+      as.matrix() %>% 
+      set_colnames(c(" ", rep(c("Estimate (Std.Err.)", "P-value"),2)))
+    
+  } else{
+    x_padj <- x_complete %>% 
+      mutate(
+        h2_padj = p.adjust(h2_p, padj_method),
+        c2_padj = p.adjust(c2_p, padj_method)
+        
+        
+      )
+    
+    x2 <- x_padj %>% 
+      mutate(
+        Estimate = paste0(round(h2, 3), " (", round(h2_SE, 3), ")"),
+        Estimate2 = paste0(round(c2, 3), " (", round(c2_SE, 3), ")")
+      ) %>% 
+      select(trait, Estimate, h2_padj, Estimate2, c2_padj) %>% 
+      mutate_if(is.numeric, format.pval, digits = 1) %>%
+      as_tibble() %>% 
+      as.matrix() %>% 
+      set_colnames(c("Trait", rep(c("Estimate (Std.Err.)", paste0("Q-value", " (", padj_method, ")")),2)))
+  }
+  
+  
   
   kable_basic <- kbl(x2) %>% 
     kable_styling() %>% 
     add_header_above(c(" " = 1, "h²" = 2, "c²" = 2))
   
-  if(is.numeric(highlight_significant)){
-    kable_basic %>% 
-      row_spec(which(x$h2_p < highlight_significant), bold = T) %>% 
-      row_spec(which(x$h2_p > highlight_significant), color = "lightgray") %>% 
-      return()
-    
-  }else{
-    return(kable_basic)
-  }
+  
+  return(kable_basic)
+  
 }
